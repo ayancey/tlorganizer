e9c5b62f5c4dc135693596e2ff06adf512fd1dc4
Alex Yancey <alexyancey3@gmail.com>
2015-02-09 13:09:20 -0800

    Proof of concept? Detects "anime", preserves file name, moves to Documents.

--- scan.py
+++ scan.py
@@ -1,9 +1,17 @@
+# This looks ugly
 from dirmon.dirmon import DirectoryMonitor
+import shutil
 
-monitor = DirectoryMonitor(".", 5)
+# http://www.pythoncentral.io/how-to-rename-move-a-file-in-python/
+def move(src, dest):
+    shutil.move(src, dest)
+
+monitor = DirectoryMonitor("C:\Users\Alex\Downloads", 1)
 
 def added(file):
 	print('Added: ' + file)
+	if "anime" in file:
+		move("C:\Users\Alex\Downloads\\" + file, "C:\Users\Alex\Documents\\" + file)
 
 def removed(file):
 	print('Removed: ' + file)
