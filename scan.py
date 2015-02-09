64b795132f89e98fb0b0b6b71de68945549a7c5a
Alex Yancey <alexyancey3@gmail.com>
2015-02-09 12:53:36 -0800

    Added dirmon as a submodule and made sure it worked

--- scan.py
+++ scan.py
@@ -1,41 +1,13 @@
-import os
-import time
+from dirmon.dirmon import DirectoryMonitor
 
-class DirectoryMonitor:
-	directory = ''
-	on_added = None
-	on_removed = None
+monitor = DirectoryMonitor(".", 5)
 
-	def __init__(self, path_i):
-		self.directory = path_i
+def added(file):
+	print('Added: ' + file)
 
-	def start(self):
-		before = os.listdir(self.directory)
+def removed(file):
+	print('Removed: ' + file)
 
-		time.sleep(5)
-
-		after = os.listdir(self.directory)
-
-		for file in before:
-			pass
-			#`print file
-
-		removed = list(set(before) - set(after))
-		for file in removed:
-			self.on_removed(file)
-
-		added = list(set(after) - set(before))
-		for file in added:
-			self.on_added(file)
-
-def ohman(thefile):
-	print 'Added: ' + thefile
-
-def ohman2(thefile):
-	print 'Removed: ' + thefile
-
-this = DirectoryMonitor('C:\Users\Alex\Downloads')
-this.on_added = ohman
-this.on_removed = ohman2
-this.start()
-
+monitor.on_added = added
+monitor.on_removed = removed
+monitor.start()
