e6b0011a5fc59c509c7872362a9dd29ddb1f82ab
Alex Yancey <alexyancey3@gmail.com>
2015-02-12 12:26:29 -0800

    End of class commit

--- jsonconf.py
+++ jsonconf.py
@@ -1,10 +1,19 @@
 import json
+from jsonschema import validate
 
 fi = open('example.json')
 myson = fi.read()
 fi.close()
 
+fi = open('schema/config.json')
+schema = fi.read()
+fi.close()
+
+
 config = json.loads(myson)
+schema = json.loads(schema)
+
+print validate(config, schema)
 
 for thing in config:
 	print thing + ' ' + str(config[thing])
